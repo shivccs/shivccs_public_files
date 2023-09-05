4a8b4a813322037e68851c728b56cab0bee0ac80
root <root@ip-172-31-47-103.ap-south-1.compute.internal>
2023-09-05 09:03:13 +0000

    test view

---
+++ test_view.php
@@ -0,0 +1,266 @@
+
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\yadmin\modules\process\models\School */
+
+$this->title = 'ICT Progress Report';
+?>
+<div class="school-view">
+    <div class='row'>
+        <div class='col-sm-12'>
+            <div class="box-header with-border">
+                <h3 class="box-title"><i class="fa fa-eye"></i>ICT Daily Progress Report</h3>
+                <div class="box-tools pull-right">
+                    <div class="actions">
+                        <?php
+                        echo Html::a('<i class="fa fa-list"></i>Shared Data List', ["index"], ['class' => 'btn btn-xs btn-primary']);
+                        ?>
+                    </div>
+                </div>
+            </div>
+
+	   <div class="box box-info">
+                <div class='box-body box-no-padding'>
+                    <div class='box-body box-no-padding'>
+                        <div class="row text-center">
+                           <h3><u>ICT Progress Report </u></h3>
+                           <h3><u>Shared API Data</u></h3>
+                        </div>
+                        <br>
+                        <div class="row">
+                          <div class="col-md-12">
+                        </div>
+                        <br>
+	  		
+		 </div>
+	     </div>
+	  </div>
+
+	  <div class="box box-info">
+                <div class="box-body box-no-padding ">
+                    <div class="box-body box-no-padding profileWrap">
+
+			<div class="row text-center">
+                           <h5><b>A. ICT Lab Established/Functional Status: </b></h5>
+                        </div>
+			<div class="row">
+                          <div class="col-md-12">
+                                <table class="table table-striped">
+                                        <thead>
+                                                <tr>
+                                                        <th></th>
+                                                        <th width="20%">No. of Labs Established</th>
+                                                        <th width="20%">No. of Labs Not Established</th>
+                                                        <th width="20%">Total Functional Today</th>
+                                                        <th width="20%">Total Non-Functional Today</th>
+                                                </tr>
+                                        </thead>
+					<tbody>
+                                                <tr>
+                                                        <td>ICT Lab Established /Functional</td>
+                                                        <td>Desktop with Monitor</td>
+                                                        <td>1</td>
+                                                        <td></td>
+                                                        <td></td>
+                                                </tr>
+					</tbody>
+				</table>
+			  </div>
+			</div>
+
+
+
+
+	            </div>
+		</div>
+	  </div>
+	  
+	 <div class="box box-info">
+                <div class='box-body box-no-padding'>
+                    <div class='box-body box-no-padding'>
+                        <div class="row text-center">
+                           <h3><u>ICT Progress Report </u></h3>
+                           <h3><u>Shared API Data</u></h3>
+                        </div>
+                        <br>
+                        <div class="row">
+                          <div class="col-md-12">
+                        </div>
+                        <br>
+                        
+                 </div>
+             </div>
+          </div>
+
+
+
+	<div class="box box-info">
+                <div class='box-body box-no-padding'>
+                    <div class='box-body box-no-padding'>
+                        <div class="row text-center">
+                           <h3><u>ICT Progress Report </u></h3>
+                           <h3><u>Shared API Data</u></h3>
+                        </div>
+                        <br>
+                        <div class="row">
+                          <div class="col-md-12">
+                        </div>
+                        <br>
+                        
+                 </div>
+             </div>
+          </div>
+
+
+	<div class="box box-info">
+                <div class='box-body box-no-padding'>
+                    <div class='box-body box-no-padding'>
+                        <div class="row text-center">
+                           <h3><u>ICT Progress Report </u></h3>
+                           <h3><u>Shared API Data</u></h3>
+                        </div>
+                        <br>
+                        <div class="row">
+                          <div class="col-md-12">
+                        </div>
+                        <br>
+                        
+                 </div>
+             </div>
+          </div>
+
+
+
+	<div class="box box-info">
+                <div class='box-body box-no-padding'>
+                    <div class='box-body box-no-padding'>
+                        <div class="row text-center">
+                           <h3><u>ICT Progress Report </u></h3>
+                           <h3><u>Shared API Data</u></h3>
+                        </div>
+                        <br>
+                        <div class="row">
+                          <div class="col-md-12">
+                        </div>
+                        <br>
+                        
+                 </div>
+             </div>
+          </div>
+
+
+
+	<div class="box box-info">
+                <div class='box-body box-no-padding'>
+                    <div class='box-body box-no-padding'>
+                        <div class="row text-center">
+                           <h3><u>ICT Progress Report </u></h3>
+                           <h3><u>Shared API Data</u></h3>
+                        </div>
+                        <br>
+                        <div class="row">
+                          <div class="col-md-12">
+                        </div>
+                        <br>
+                        
+                 </div>
+             </div>
+          </div>
+
+
+
+	<div class="box box-info">
+                <div class='box-body box-no-padding'>
+                    <div class='box-body box-no-padding'>
+                        <div class="row text-center">
+                           <h3><u>ICT Progress Report </u></h3>
+                           <h3><u>Shared API Data</u></h3>
+                        </div>
+                        <br>
+                        <div class="row">
+                          <div class="col-md-12">
+                        </div>
+                        <br>
+                        
+                 </div>
+             </div>
+          </div>
+
+
+	<div class="box box-info">
+                <div class='box-body box-no-padding'>
+                    <div class='box-body box-no-padding'>
+                        <div class="row text-center">
+                           <h3><u>ICT Progress Report </u></h3>
+                           <h3><u>Shared API Data</u></h3>
+                        </div>
+                        <br>
+                        <div class="row">
+                          <div class="col-md-12">
+                        </div>
+                        <br>
+                        
+                 </div>
+             </div>
+          </div>
+
+
+	<div class="box box-info">
+                <div class='box-body box-no-padding'>
+                    <div class='box-body box-no-padding'>
+                        <div class="row text-center">
+                           <h3><u>ICT Progress Report </u></h3>
+                           <h3><u>Shared API Data</u></h3>
+                        </div>
+                        <br>
+                        <div class="row">
+                          <div class="col-md-12">
+                        </div>
+                        <br>
+                        
+                 </div>
+             </div>
+          </div>
+
+
+	<div class="box box-info">
+                <div class='box-body box-no-padding'>
+                    <div class='box-body box-no-padding'>
+                        <div class="row text-center">
+                           <h3><u>ICT Progress Report </u></h3>
+                           <h3><u>Shared API Data</u></h3>
+                        </div>
+                        <br>
+                        <div class="row">
+                          <div class="col-md-12">
+                        </div>
+                        <br>
+                        
+                 </div>
+             </div>
+          </div>
+
+
+	<div class="box box-info">
+                <div class='box-body box-no-padding'>
+                    <div class='box-body box-no-padding'>
+                        <div class="row text-center">
+                           <h3><u>ICT Progress Report </u></h3>
+                           <h3><u>Shared API Data</u></h3>
+                        </div>
+                        <br>
+                        <div class="row">
+                          <div class="col-
+
+
+
+
+
+
+    	</div>
+    </div>
+</div>
